81466ea6abab24799b16ef4428c37608cac765e6
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-11-14 13:10:15 +0100

    Small fixes

--- src/main/utils/update_utils.py
+++ src/main/utils/update_utils.py
@@ -1,49 +1,62 @@
 import main.ulogging as ulogging
 
-try:
-    with open('.updateRequested', "r") as updateRequested:
-        ulogging.info('Update requested...')
-        connectToWifi()
-        updateTimeUsingNTP()
-        otaUpdate()
-        ulogging.info('Updates finished, will reboot')
 
-    os.remove('.updateRequested')
-    machine.reset()
+class UpdateUtils:
 
-except:
-    ulogging.info('No update needed')
-    pass
+    @staticmethod
+    def updateIfNecessary():
+        try:
+            with open('.updateRequested', "r") as updateRequested:
+                try:
+                    ulogging.info('Update requested...')
+                    UpdateUtils._connectToWifi()
+                    UpdateUtils._updateTimeUsingNTP()
+                    UpdateUtils._otaUpdate()
+                    ulogging.info('Updates finished, will reboot')
+                except BaseException as error:
+                    print(error)
+                    ulogging.error('Error updating: '+ str(error))
+
+            os.remove('.updateRequested')
+            machine.reset()
 
 
-def connectToWifi():
-    import utime, network, main.secrets as secrets
-    from main.utils.httpclient import HttpClient
-    ulogging.info('Connecting to WIFI')
+        except BaseException as error:
+            print(error)
+            ulogging.info('No update needed')
+            pass
 
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+    @staticmethod
+    def _connectToWifi():
+        import utime, network, main.secrets as secrets
+        from main.utils.httpclient import HttpClient
+        ulogging.info('Connecting to WIFI')
 
-    startTime = utime.time()
-    while not sta_if.isconnected():
-        if startTime + 45 < utime.time():
-            break
-
-        utime.sleep(2)
+        sta_if = network.WLAN(network.STA_IF)
+        sta_if.active(True)
         sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-    ulogging.info('Connected to WIFI')
 
+        startTime = utime.time()
+        while not sta_if.isconnected():
+            if startTime + 45 < utime.time():
+                break
 
-def updateTimeUsingNTP():
-    ulogging.info('Updating time...')
-    import machine
-    rtc = machine.RTC()
-    rtc.ntp_sync(server="0.be.pool.ntp.org", tz="CET-1CEST,M3.5.0,M10.5.0/3")
-    ulogging.info('Updated time...')
+            utime.sleep(2)
+            sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        ulogging.info('Connected to WIFI')
 
+    @staticmethod
+    def _updateTimeUsingNTP():
+        ulogging.info('Updating time...')
+        import machine
+        rtc = machine.RTC()
+        rtc.ntp_sync(server="0.be.pool.ntp.org", tz="CET-1CEST,M3.5.0,M10.5.0/3")
+        ulogging.info('Updated time...')
 
-def otaUpdate():
-    from .ota_updater import OTAUpdater
-    otaUpdater = OTAUpdater('https://github.com/rdehuyss/chicken-shed-mgr')
-    otaUpdater.download_update_if_available()
+    @staticmethod
+    def _otaUpdate():
+        from .ota_updater import OTAUpdater
+        otaUpdater = OTAUpdater('https://github.com/rdehuyss/chicken-shed-mgr')
+        otaUpdater.download_update_if_available()
+
+UpdateUtils.updateIfNecessary()
