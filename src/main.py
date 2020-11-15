6146058758345500478e39cfcc319f0617c9f94a
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-11-15 14:08:41 +0100

    Workaround for Pymakr issue

--- src/main.py
+++ src/main.py
@@ -4,5 +4,5 @@ if utime.localtime()[0] < 2000:
     rtc = machine.RTC()
     rtc.init((2020, 11, 10, 22, 9, 5))
 
-#import main.utils.update_utils
-#import main.start
+import app.utils.update_utils
+import app.start
