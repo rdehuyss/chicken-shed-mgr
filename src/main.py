6d512bd1d84532e66cb2fb12455aa524faa5e2dc
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-11-16 09:56:00 +0100

    Small update

--- src/main.py
+++ src/main.py
@@ -6,10 +6,3 @@ if utime.localtime()[0] < 2000:
 
 import app.utils.update_utils
 import app.start
-
-from app.utils.ota_logger import OTALogger
-
-o = OTALogger('b5b29d316b4a0f61a8be264f236a2376', '245f173c2937fec397b5b58dbf0e97e4b5360520')
-
-
-
