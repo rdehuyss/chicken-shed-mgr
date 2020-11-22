adf5efe1f0d126b4839c194e5eea07928f5b9b89
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-11-22 07:26:24 +0100

    Small fix if sensor is not connected

--- src/app/hardware/light_sensor.py
+++ src/app/hardware/light_sensor.py
@@ -9,9 +9,12 @@ class LightSensor():
     
     def __init__(self, updateRate:int = 60):
         i2c = machine.I2C(freq=400000, sda=21, scl=22)
-        self.bh1750 = BH1750(i2c)
-        self.updateRate = updateRate
-        self.lastLightSensorMeasurementTime = 0
+        try:
+            self.bh1750 = BH1750(i2c)
+            self.updateRate = updateRate
+            self.lastLightSensorMeasurementTime = 0
+        except OSError:
+            self.bh1750 = None
         self.currentLightSensorValue = -1
 
     def luminance(self):
@@ -23,6 +26,9 @@ class LightSensor():
         return self.currentLightSensorValue
 
     def getLuminanceFromSensor(self):
+        if self.bh1750 is None:
+            return -1
+            
         try:
             return int(self.bh1750.luminance(BH1750.ONCE_HIRES_1))        
         except:
